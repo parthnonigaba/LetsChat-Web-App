f3c8817f5f0d24168cf9a510febf82ff080070a7
parthnonigaba <85085655+parthnonigaba@users.noreply.github.com>
2022-01-17 10:51:00 -0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
     user_name = localStorage.getItem("user_name");
     room_name = localStorage.getItem("room_name");
-    document.getElementById("user_name").innerHTML = "Welcome" + user_name + "!";
+    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
     function addRoom(){
           room_name = document.getElementById("room_name").value;
           firebase.database().ref("/").child(room_name).update({
